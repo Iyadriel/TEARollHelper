3c2af00619c7b217d139e42d52355f122b0790ef
Iyadriel <bart.corremans+iyadrielgithub@gmail.com>
2020-05-10 14:10:03 +0200

    Prep turns module for roll UI

--- turns.lua
+++ turns.lua
@@ -3,6 +3,7 @@ local _, ns = ...
 local actions = ns.actions
 local events = ns.events
 local rules = ns.rules
+local turns = ns.turns
 
 local TURN_MODE_ATTACK = 0
 local TURN_MODE_DEFEND = 1
@@ -11,15 +12,17 @@ local BUFF_TYPES = {
     DEFENCE = "defence"
 }
 
-local setTurnMode, getCurrentTurnValues, clearCurrentTurnValues, handleRollResult
+local setTurnMode, setCurrentRoll, getCurrentTurnValues, handleRollResult
+local setAttackValues, setDefendValues
 local startAttackTurn, startDefendTurn
 local getCurrentBuffs, setCurrentBuff, clearCurrentBuffs, expireCurrentBuff
 
 local currentTurnValues = {
     turnMode = TURN_MODE_ATTACK,
-    attackThreshold = nil,
-    defendTreshold = nil,
-    damageRisk = nil
+    roll = 1,
+    attackThreshold = 0,
+    defendThreshold = 0,
+    damageRisk = 0
 }
 
 local currentBuffs = {
@@ -31,46 +34,52 @@ function setTurnMode(mode)
     currentTurnValues.turnMode = mode
 end
 
-function getCurrentTurnValues()
-    return {
-        attackThreshold = currentTurnValues.attackThreshold,
-        defendTreshold = currentTurnValues.defendTreshold,
-        damageRisk = currentTurnValues.damageRisk
-    }
+function setCurrentRoll(roll)
+    currentTurnValues.roll = roll
 end
 
-function clearCurrentTurnValues()
-    currentTurnValues.attackThreshold = nil
-    defendTreshold = nil
-    damageRisk = nil
+function getCurrentTurnValues()
+    return currentTurnValues
+end
+
+function setAttackValues(attackThreshold)
+    currentTurnValues.attackThreshold = attackThreshold
+end
+
+function setDefendValues(defendThreshold, damageRisk)
+    if defendThreshold ~= nil then
+        currentTurnValues.defendThreshold = defendThreshold
+    end
+    if damageRisk ~= nil then
+        currentTurnValues.damageRisk = damageRisk
+    end
 end
 
 function startAttackTurn(attackThreshold)
     events.listenForRolls()
 
     setTurnMode(TURN_MODE_ATTACK)
-    currentTurnValues.attackThreshold = attackThreshold
+    setAttackValues(attackThreshold)
 
     RandomRoll(1, rules.MAX_ROLL)
 end
 
-function startDefendTurn(defendTreshold, damageRisk)
+function startDefendTurn(defendThreshold, damageRisk)
     events.listenForRolls()
 
     setTurnMode(TURN_MODE_DEFEND)
-    currentTurnValues.defendTreshold = defendTreshold
-    currentTurnValues.damageRisk = damageRisk
+    setDefendValues(defendThreshold, damageRisk)
 
     RandomRoll(1, rules.MAX_ROLL)
 end
 
 function handleRollResult(roll)
+    setCurrentRoll(roll)
     if currentTurnValues.turnMode == TURN_MODE_ATTACK then
         actions.performAttack(roll)
     elseif currentTurnValues.turnMode == TURN_MODE_DEFEND then
         actions.performDefence(roll)
     end
-    clearCurrentTurnValues()
 end
 
 function getCurrentBuffs()
@@ -81,8 +90,12 @@ function getCurrentBuffs()
 end
 
 function setCurrentBuff(buffType, amount)
-        currentBuffs[buffType] = amount
-    end
+    currentBuffs[buffType] = amount
+end
+
+function clearCurrentBuffs()
+    setCurrentBuff(BUFF_TYPES.OFFENCE, 0)
+    setCurrentBuff(BUFF_TYPES.DEFENCE, 0)
 end
 
 function expireCurrentBuff(buffType)
@@ -92,11 +105,15 @@ function expireCurrentBuff(buffType)
     currentBuffs[buffType] = 0
 end
 
-ns.turns.BUFF_TYPES = BUFF_TYPES
-ns.turns.getCurrentTurnValues = getCurrentTurnValues
-ns.turns.startAttackTurn = startAttackTurn
-ns.turns.startDefendTurn = startDefendTurn
-ns.turns.handleRollResult = handleRollResult
-ns.turns.getCurrentBuffs = getCurrentBuffs
-ns.turns.setCurrentBuff = setCurrentBuff
-ns.turns.clearCurrentBuff = clearCurrentBuff
+turns.BUFF_TYPES = BUFF_TYPES
+turns.getCurrentTurnValues = getCurrentTurnValues
+turns.setCurrentRoll = setCurrentRoll
+turns.setAttackValues = setAttackValues
+turns.setDefendValues = setDefendValues
+turns.startAttackTurn = startAttackTurn
+turns.startDefendTurn = startDefendTurn
+turns.handleRollResult = handleRollResult
+turns.getCurrentBuffs = getCurrentBuffs
+turns.setCurrentBuff = setCurrentBuff
+turns.clearCurrentBuffs = clearCurrentBuffs
+turns.expireCurrentBuff = expireCurrentBuff
