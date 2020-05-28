13559ef57789300b8ced905dfe5502c6737d0f15
Iyadriel <bart.corremans+iyadrielgithub@gmail.com>
2020-05-28 16:09:55 +0200

    Add Corrupted weakness definition (WIP)

--- resources/weaknesses.lua
+++ resources/weaknesses.lua
@@ -1,13 +1,19 @@
 local _, ns = ...
 
 local weaknesses = ns.resources.weaknesses
-weaknesses.WEAKNESS_KEYS = {"FATELESS", "FRAGILE", "OUTCAST"}
+weaknesses.WEAKNESS_KEYS = {"CORRUPTED", "FATELESS", "FRAGILE", "OUTCAST"}
 
 weaknesses.WEAKNESSES = {
 --[[     BRUTE = {
         id = "BRUTE",
         name = "Brute",
         desc = "You can no longer heal, buff, or pick any Utility traits.",
+        supported = true,
+    }, ]]
+--[[     CORRUPTED = {
+        id = "CORRUPTED",
+        name = "Corrupted",
+        desc = "All healing received from other players and NPCs are cut in half, rounded down. Furthermore you must choose one of the following types of healing: Holy, Unholy, Life. Whenever you are healed by the chosen type, your max HP is reduced by 3. Your HP returns to normal after combat ends, but you are not healed for the missing HP. If healed by your chosen type outside of combat your HP returns to normal after the next combat section ends.",
         supported = true,
     }, ]]
     FATELESS = {
