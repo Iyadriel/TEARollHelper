4030a083934a5771bc22761908f0e0ded93bb28d
Iyadriel <bart.corremans+iyadrielgithub@gmail.com>
2020-06-08 09:29:14 +0200

    Move roll rules to separate file

---
+++ rules/rolls.lua
@@ -0,0 +1,73 @@
+local _, ns = ...
+
+local character = ns.character
+local constants = ns.constants
+local feats = ns.resources.feats
+local rules = ns.rules
+local weaknesses = ns.resources.weaknesses
+
+local ACTIONS = constants.ACTIONS
+local FEATS = feats.FEATS
+local TURN_TYPES = constants.TURN_TYPES
+local WEAKNESSES = weaknesses.WEAKNESSES
+
+local MIN_ROLL = 1
+local MAX_ROLL = 20
+
+local function getCritReq()
+    local critReq = MAX_ROLL
+    if character.hasFeat(FEATS.KEEN_SENSE) then
+        critReq = critReq - 1
+    end
+    return critReq
+end
+
+local function getMaxFatePoints()
+    return character.hasWeakness(WEAKNESSES.FATELESS) and 0 or 1
+end
+
+local function getRollModeModifier(action, advantageBuff, disadvantageDebuff, enemyId)
+    local modifier = 0
+
+    if advantageBuff then
+        modifier = modifier + 1
+    end
+    if disadvantageDebuff then
+        modifier = modifier - 1
+    end
+
+    if action == ACTIONS.attack then
+        modifier = modifier + rules.offence.getRollModeModifier(enemyId)
+    end
+
+    return modifier
+end
+
+local function canProcRebound()
+    return character.hasWeakness(WEAKNESSES.REBOUND)
+end
+
+local function validateStatsForRebound()
+    return max(character.getPlayerOffence(), character.getPlayerSpirit()) >= 4
+end
+
+local function hasReboundProc(roll, turnTypeId)
+    return turnTypeId == TURN_TYPES.PLAYER.id and roll == MIN_ROLL
+end
+
+local function calculateReboundDamage()
+    return max(character.getPlayerOffence(), character.getPlayerSpirit())
+end
+
+rules.rolls = {
+    MIN_ROLL = MIN_ROLL,
+    MAX_ROLL = MAX_ROLL,
+
+    getCritReq = getCritReq,
+    getMaxFatePoints = getMaxFatePoints,
+    getRollModeModifier = getRollModeModifier,
+    validateStatsForRebound = validateStatsForRebound,
+    canProcRebound = canProcRebound,
+    hasReboundProc = hasReboundProc,
+    calculateReboundDamage = calculateReboundDamage,
+}
