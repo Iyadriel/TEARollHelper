526b33456581563c194c8ac8ea42fbd5f0556eb9
Iyadriel <bart.corremans+iyadrielgithub@gmail.com>
2020-10-27 13:03:11 +0100

    Remove unused feat "supported" property

--- resources/feats.lua
+++ resources/feats.lua
@@ -19,19 +19,16 @@ feats.FEATS = {
         id = "ADRENALINE",
         name = "Adrenaline",
         desc = "Beating the threshold by 6 or more with an Offence attack roll lets you perform a second attack against the same target. Cannot trigger more than once per player turn.",
-        supported = true
     },
     BLOOD_HARVEST = {
         id = "BLOOD_HARVEST",
         name = "Blood Harvest",
         desc = "For every 2 points you put into the Offence stat you gain a Harvest Slot. You can activate these Harvest Slots to spend them just like a Greater Heal Slot. Spending a Harvest slot increases the damage of your next Offence attack by +3. This damage is dealt even if you miss.",
-        supported = true
     },
     COUNTER_FORCE = {
         id = "COUNTER_FORCE",
         name = "Counter-Force",
         desc = "Your melee save rolls no longer benefit from your Defence stat, but if you manage the roll you deal damage back to the attacker by an amount equal to your Defence stat.",
-        supported = true
     },
     ETERNAL_SACRIFICE = {
         id = "ETERNAL_SACRIFICE",
@@ -43,7 +40,6 @@ feats.FEATS = {
                 [DAMAGE_TYPES.MIXED] = 2,
             }
         },
-        supported = true,
         isCustom = true,
         player = "KELANRA",
     },
@@ -51,32 +47,28 @@ feats.FEATS = {
         id = "EXPANSIVE_ARSENAL",
         name = "Expansive Arsenal",
         desc = "If you pick a second Weakness, you may pick a third Trait.",
-        supported = true
     },
     FOREIGN_DISCIPLE = {
         id = "FOREIGN_DISCIPLE",
         name = "Foreign Disciple",
-        desc = "You can change your Racial Trait to that of another race.",
-        supported = true
+        desc = "You can now pick a second racial trait in addition to your first. Cannot be picked with Outcast.",
+        supported = false
     },
     INSPIRING_PRESENCE = {
         id = "INSPIRING_PRESENCE",
         name = "Inspiring Presence",
         desc = "You now buff someone for both their current player turn and the next enemy turn, but you only apply half of your spirit stat to the roll, rounded up.",
-        supported = true
     },
     KEEN_SENSE = {
         id = "KEEN_SENSE",
         name = "Keen sense",
         desc = "The threshold for getting a critical roll is reduced to 19 from 20.",
-        supported = true
     },
     LEADER = {
         id = "LEADER",
         name = "Leader",
         desc = "You can now buff with the Offence stat instead of the Spirit stat.",
         note = "The addon will automatically use the highest stat.",
-        supported = true
     },
     LIVING_BARRICADE = {
         id = "LIVING_BARRICADE",
@@ -84,7 +76,6 @@ feats.FEATS = {
         desc = "When tasked with doing multiple defense rolls in the same enemy turn (saves not included) you take 3 less damage from all sources for the duration of the enemy turn.",
         note = "Activate manually from the Defend action tab.",
         icon = "Interface\\Icons\\ability_warrior_shieldwall",
-        supported = true,
         buff = {
             type = BUFF_TYPES.DAMAGE_TAKEN,
             amount = -3,
@@ -97,20 +88,17 @@ feats.FEATS = {
         id = "MEDIC",
         name = "Medic",
         desc = "Your out of combat basic healing is doubled, and the amount of heals you can do out of combat is increased by 2.",
-        supported = true
     },
     MENDER = {
         id = "MENDER",
         name = "Mender",
         desc = "Gain 2 additional Greater Heal Slots.",
-        supported = true
     },
     MERCY_FROM_PAIN = {
         id = "MERCY_FROM_PAIN",
         name = "Mercy from Pain",
         desc = "Every time you deal 5 damage or more to a single enemy, your next healing roll is boosted by +2 HP, if you deal 5 damage or more to multiple enemies at once, your next healing roll is instead boosted by +4HP (does not stack). If you do not use this bonus on your next player turn, it fades.",
         icon = "Interface\\Icons\\spell_holy_holyguidance",
-        supported = true,
         buff = {
             type = BUFF_TYPES.HEALING_DONE,
             expireAfterFirstAction = {
@@ -131,49 +119,41 @@ feats.FEATS = {
                 VOID = true,
             },
         },
-        supported = true
     },
     ONSLAUGHT = {
         id = "ONSLAUGHT",
         name = "Onslaught",
         desc = "When hitting with an Offence attack roll you always deal your base damage plus half of your Offence stat rounded up, regardless of how much you beat the threshold by.",
-        supported = true
     },
     PARAGON = {
         id = "PARAGON",
         name = "Paragon",
         desc = "You no longer gain Greater Heal Slots. Every 3 Spirit lets you apply your basic heal in full to an additional target. Not increased by buffs, but is increased by spending Excess. You can no longer divide your healing done.",
-        supported = true
     },
     PHALANX = {
         id = "PHALANX",
         name = "Phalanx",
         desc = "You now have advantage on Melee save rolls.",
-        supported = true
     },
     PROFESSIONAL = {
         id = "PROFESSIONAL",
         name = "Professional",
         desc = "You may pick 2 additional Utility Traits. Furthermore your Utility Traits now grant you a bonus of +8 rather than +5.",
-        supported = true
     },
     REAPER = {
         id = "REAPER",
         name = "Reaper",
         desc = "When scoring a nat 20 on an Offence attack roll, you no longer deal double damage, but instead activate the Reap trait without cost. You do not have to have Reap as a chosen trait.",
-        supported = true
     },
     SHEPHERD_OF_THE_WICKED = {
         id = "SHEPHERD_OF_THE_WICKED",
         name = "Shepherd of the Wicked",
         desc = "You can now roll CC rolls with your Defence stat instead of your Offence stat.",
         note = "The addon will automatically use the highest stat.",
-        supported = true
     },
     WARDER = {
         id = "WARDER",
         name = "Warder",
         desc = "You now have advantage on Ranged save rolls.",
-        supported = true
     },
 }
