adfe2eb497861ca1b031743e6d771efbb0137b55
Iyadriel <bart.corremans+iyadrielgithub@gmail.com>
2020-10-28 12:34:14 +0100

    Remove unused trait "supported" property

--- resources/traits.lua
+++ resources/traits.lua
@@ -13,13 +13,11 @@ traits.TRAITS = {
     OTHER = {
         id = "OTHER",
         name = "Other",
-        supported = true
     },
     ASCEND = {
         id = "ASCEND",
         name = "Ascend",
         desc = "Activate to replicate your buff roll and apply it to a secondary target. Can be used twice per event. Activate after rolling.",
-        supported = true,
         numCharges = 2,
     },
     BULWARK = {
@@ -27,7 +25,6 @@ traits.TRAITS = {
         name = "Bulwark",
         desc = "Activate to gain +3 to defense as well as advantage on all defense rolls for the current or next enemy turn. Can be used twice per event. Activate and then roll.",
         icon = "Interface\\Icons\\spell_holy_greaterblessingofsanctuary",
-        supported = true,
         numCharges = 2,
         buffs = {
             {
@@ -49,7 +46,6 @@ traits.TRAITS = {
         name = "Calamity Gambit",
         desc = "Activate to double your Offence stat for the current and the next player turn. However, your defence stat is reduced by an amount equal to your regular offense stat for the next two enemy turns. Can be used once per event. Activate and then roll.",
         icon = "Interface\\Icons\\spell_shadow_unstableaffliction_3",
-        supported = true,
         numCharges = 1,
         buffs = {
             {
@@ -77,7 +73,6 @@ traits.TRAITS = {
         name = "Empowered Blades",
         desc = "Activate after a successful Defence roll against a magical attack to make your next successful attack deal additional Chaos damage equal to half of the prevented damage rounded up. Can be used twice per event. Activate after rolling.",
         icon = "Interface\\Icons\\ability_demonhunter_chaosstrike",
-        supported = true,
         numCharges = 2,
         buffs = {
             {
@@ -96,7 +91,6 @@ traits.TRAITS = {
         name = "Faelune's Regrowth",
         desc = "Activate to have your Healing roll replicate half of its healing amount rounded up for the next enemy turn, and the following player turn in addition to the full healing on the current player turn. Can be used thrice per event. Activate after rolling.",
         icon = "Interface\\Icons\\ability_druid_nourish",
-        supported = true,
         numCharges = 3,
         buffs = {
             {
@@ -109,7 +103,6 @@ traits.TRAITS = {
         id = "FAULTLINE",
         name = "Faultline",
         desc = "Activate to apply the result of an Offence attack roll onto a straight line outwards from yourself. All targets within line of sight along the line are struck. Can be used twice per event. Activate after rolling.",
-        supported = true,
         numCharges = 2,
     },
     FOCUS = {
@@ -117,7 +110,6 @@ traits.TRAITS = {
         name = "Focus",
         desc = "Activate to gain advantage to all of your rolls during the current player turn. Can be activated twice per event. Activate and then roll.",
         icon = "Interface\\Icons\\spell_nature_focusedmind",
-        supported = true,
         numCharges = 2,
         buffs = {
             {
@@ -133,7 +125,6 @@ traits.TRAITS = {
         id = "LIFE_PULSE",
         name = "Life Pulse",
         desc = "Activate to apply the result of a Heal to the target and all friendly characters in melee range of that target. Can be used twice per event. Activate after rolling.",
-        supported = true,
         numCharges = 2,
     },
     LIFE_WITHIN = {
@@ -141,7 +132,6 @@ traits.TRAITS = {
         name = "Life Within",
         desc = "Activate to increase your current and max HP by 10. Lasts until end of combat. Can be used once per event. Activate outside of rolling on either a player or enemy turn.",
         icon = "Interface\\Icons\\ability_druid_flourish",
-        supported = true,
         numCharges = 1,
         buffs = {
             {
@@ -155,14 +145,12 @@ traits.TRAITS = {
         id = "PRESENCE_OF_VIRTUE",
         name = "Presence of Virtue",
         desc = "Activate after performing a successful Melee save on a friendly target, on activation you heal the target for 5 HP and buff them for +5 for their next player turn. Can be used four times per event.",
-        supported = true,
         numCharges = 4,
     },
     SECOND_WIND = {
         id = "SECOND_WIND",
         name = "Second Wind",
         desc = "Activate outside of combat to regain 15HP. Can be used once, recharges after every combat. Activate without rolling.",
-        supported = true,
         numCharges = 1,
     },
     SHATTER_SOUL = {
@@ -170,7 +158,6 @@ traits.TRAITS = {
         name = "Shatter Soul",
         desc = "Activate after a successful attack to heal yourself for 6 HP. The target of your attack must not be mechanical. If the target is a demon, your Offence is also increased by +6 on the next player turn. Can be used thrice per event. Activate after rolling.",
         icon = "Interface\\Icons\\ability_demonhunter_shatteredsouls",
-        supported = true,
         numCharges = 3,
         buffs = {
             {
@@ -190,7 +177,6 @@ traits.TRAITS = {
         id = "SHIELD_SLAM",
         name = "Shield Slam",
         desc = "Activate on a player turn, instead of rolling for attack you deal your base damage plus your Defence as damage to an enemy of your choice. Can be used thrice per event. Activate outside of rolling.",
-        supported = true,
         numCharges = 3,
     },
     VERSATILE = {
@@ -198,7 +184,6 @@ traits.TRAITS = {
         name = "Versatile",
         desc = "Activate to choose one stat, and transfer its value to another stat of your choice for the duration of your next roll. Can be used twice per event. Activate before rolling. Does not grant additional Greater Heal Slots. If you use Versatile to gain HP, and then have that amount or less HP left total by the end of your turn, you go down to 1 HP rather than 0 HP.",
         icon = "Interface\\Icons\\spell_arcane_arcanetactics",
-        supported = true,
         numCharges = 2,
         buffs = {
             {
@@ -212,7 +197,6 @@ traits.TRAITS = {
         id = "VINDICATION",
         name = "Vindication",
         desc = "Activate after a successful Offence attack roll in order to replicate half the damage done rounded up as healing. You can spread this healing as you wish among friendly targets. Can be used twice per event.",
-        supported = true,
         numCharges = 2,
     },
 }
