30b1782d26fedbf28f67cc218a943027cc52078e
Iyadriel <bart.corremans+iyadrielgithub@gmail.com>
2021-04-24 14:06:58 +0200

    Only show KO roll tab on player turn

--- ui/actions/ko.lua
+++ ui/actions/ko.lua
@@ -2,11 +2,13 @@ local _, ns = ...
 
 local characterState = ns.state.character
 local constants = ns.constants
+local turnState = ns.state.turn
 local ui = ns.ui
 
 local CONSCIOUSNESS_STATES = constants.CONSCIOUSNESS_STATES
 local SPECIAL_ACTIONS = constants.SPECIAL_ACTIONS
 local SPECIAL_ACTION_LABELS = constants.SPECIAL_ACTION_LABELS
+local TURN_TYPES = constants.TURN_TYPES
 
 --[[ local options = {
     order: Number,
@@ -20,6 +22,7 @@ ui.modules.actions.modules.KO.getOptions = function(options)
         end,
         hidden = function()
             return characterState.state.consciousness.get() ~= CONSCIOUSNESS_STATES.FADING
+                or turnState.state.type.get() ~= TURN_TYPES.PLAYER.id
         end,
         args = {
             roll = ui.modules.turn.modules.roll.getOptions({ order = 0, action = SPECIAL_ACTIONS.clingToConsciousness }),
